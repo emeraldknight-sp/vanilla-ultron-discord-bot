56eb672784d397b539837b557f0eaca12a902333
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 15:09:17 -0300

    ✅ Add: bot status

---
+++ src/status.js
@@ -0,0 +1,25 @@
+export const status = [
+  {
+    name: "Use / - Nerd Strike",
+    type: "STREAMING",
+    url: "https://www.youtube.com/channel/UCwZYI1VnymmuL424TeWoFRw",
+  },
+
+  {
+    name: `_emeraldknight`,
+    type: "STREAMING",
+    url: "https://twitch.tv/emeraldknightofc",
+  },
+
+  {
+    name: "https.fb.com/NerdStrike",
+    type: "STREAMING",
+    url: "https://facebook.com/NerdStrike",
+  },
+
+  {
+    name: "prefixo: /",
+    type: "STREAMING",
+    url: "https://www.youtube.com/channel/UCwZYI1VnymmuL424TeWoFRw",
+  },
+];
