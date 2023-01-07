53bb962539fdd97d983bef4dda5031a96fa44380
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 15:09:36 -0300

    ✅ Add: ping, server, user bot commands

---
+++ src/commands/ping.js
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export default function () {
+  return {
+    data: new SlashCommandBuilder()
+      .setName("ping")
+      .setDescription("Replies with pong!"),
+    async execute(interaction) {
+      await interaction.reply("Pong!");
+    },
+  };
+}
