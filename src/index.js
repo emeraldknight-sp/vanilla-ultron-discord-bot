5b76144101b3503bd99c133b1a8cbb07fcdf19ff
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 18:41:32 -0300

    ✅ Update: index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import { config } from "./config.js";
 import { status } from "./status.js";
-import path from "node:path";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import fs from "node:fs";
 
 const client = new Client({
@@ -15,10 +16,58 @@ const client = new Client({
 
 client.commands = new Collection();
 
-const commandsPath = path.join
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-client.on("ready", () => {
-  console.log("Bot conectado com sucesso!");
+const commandsPath = path.join(__dirname, "/commands");
+
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+
+  // IMPORTANDO O ARQUIVO
+  const command = import(`${filePath}`)
+    .then((param) => {
+      if ("data" in command && "execute" in command) {
+        client.commands.set(param.data.name, param);
+        console.log(client.commands);
+      }
+    })
+    .catch(() => {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    });
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.once(Events.ClientReady, (clientReady) => {
+  console.log(
+    `Ready! Device successfully connected. Hello there, I am ${clientReady.user.tag}!`
+  );
+
   const setStatus = () => {
     let randomStatus = status[Math.floor(Math.random() * status.length)];
     client.user.setPresence({ game: randomStatus });
@@ -26,11 +75,6 @@ client.on("ready", () => {
 
   setStatus();
   setInterval(() => setStatus(), 10000);
-});
-
-client.on("guildCreate", (guild) => {
-  console.log(`O bot entrou no servidor: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers.`);
 });
 
 client.login(config.token);
