c28720fa983a70b4614aa90888d2fc717e426966
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 23:08:41 -0300

    ✅ Update: module.exports

--- src/commands/user.js
+++ src/commands/user.js
@@ -1,13 +1,13 @@
-import { SlashCommandBuilder } from "discord.js";
+const { SlashCommandBuilder } = require("discord.js");
 
-export const user = {
+module.exports = {
   data: new SlashCommandBuilder()
     .setName("user")
     .setDescription("Provides information about the user"),
 
   async execute(interaction) {
     await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+      `This command was run by @${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
     );
   },
 };
